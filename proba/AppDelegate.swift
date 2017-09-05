f0180bfda22d196df5d19bc146799594b7f505aa
Gergo Markovits <gergomarkovits@Gergo-iMac.local>
2017-09-05 22:07:50 +0200

    Initial Commit

---
+++ proba/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  proba
+//
+//  Created by Gergo Markovits on 2017. 09. 05..
+//  Copyright © 2017. Gergo Markovits. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ probaTests/probaTests.swift
@@ -0,0 +1,36 @@
+//
+//  probaTests.swift
+//  probaTests
+//
+//  Created by Gergo Markovits on 2017. 09. 05..
+//  Copyright © 2017. Gergo Markovits. All rights reserved.
+//
+
+import XCTest
+@testable import proba
+
+class probaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
